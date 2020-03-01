8699623734218d8164f0d9c8ec4beb7d491ced59
Dominik Dancs <dodancs@moow.info>
2020-03-01 13:53:23 +0100

    Added migrations for domains, sites and canaries tables

---
+++ database/migrations/2020_03_01_130803_create_canaries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCanariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('canaries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid')->unique()->notNullable()->index();
+            $table->uuid('domain')->index();
+            $table->uuid('site')->index();
+            $table->uuid('assignee')->index();
+            $table->foreign('domain')->references('uuid')->on('domains')->notNullable()->onDelete('cascade');
+            $table->foreign('site')->references('uuid')->on('sites')->notNullable()->onDelete('cascade');
+            $table->foreign('assignee')->references('uuid')->on('users')->nullable()->default(null);
+            $table->boolean('testing')->default(false);
+            $table->json('data')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('canaries');
+    }
+}
